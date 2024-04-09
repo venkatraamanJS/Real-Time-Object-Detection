2c40772657dd36c493b483f9ed24afeeca979491
venkat <venkat@Laptop407.local>
2024-04-09 18:04:33 +0530

    added logger and exception setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'VenkatRam',
+    author_email= 'venkatraaman03@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
