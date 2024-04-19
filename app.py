d0ca415cc55ef3609bc8e0103ac2448a6017625d
venkat <venkat@Laptop407.local>
2024-04-19 19:11:02 +0530

    added code for data ingestion

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import sys, os
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 from signLanguage.exception import SignException
 from signLanguage.logger import logging
-import sys, os
 
 logging.info("Welcome to Friday-night")
 
 try:
-    a= 7/'9'
+    obj=TrainPipeline()
+    obj.run_pipeline()
 except Exception as e:
     raise SignException(e,sys) from e
 
 
- 
